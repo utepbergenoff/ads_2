b914dce9550f8baa89ac7196ac10f86107441e52
yerlanutepbergenov <utepbergenoverlan06@gmail.com>
2025-04-12 08:24:29 +0500

    1 commit

---
+++ src/MyLIst.java
@@ -0,0 +1,20 @@
+public interface MyLIst<T> extends Iterable<T> {
+    void add(T item);
+    void set(int index, T item);
+    void add(int index, T item);
+    void addFirst(T item);
+    void addLast(T item);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    void sort();
+    void indexOf(Object object);
+    int lastIndexOf(Object object);
+    boolean exists(Object object);
+    public Object[] toArray();
+    void clear();
+    int size();
+}

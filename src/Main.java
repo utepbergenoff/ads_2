6569a9e0136747940f8354b877954596ba2d1649
yerlanutepbergenov <utepbergenoverlan06@gmail.com>
2025-04-13 16:22:35 +0500

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,67 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        MyArrayList<Integer> list = new MyArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Test add
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println("After add: " + java.util.Arrays.toString(list.toArray()));
+
+        // Test add(index, item)
+        list.add(1, 15);
+        System.out.println("After insert at index 1: " + java.util.Arrays.toString(list.toArray()));
+
+        // Test get
+        System.out.println("Get index 2: " + list.get(2));
+
+        // Test set
+        list.set(2, 25);
+        System.out.println("After set index 2 to 25: " + java.util.Arrays.toString(list.toArray()));
+
+        // Test getFirst and getLast
+        System.out.println("First: " + list.getFirst());
+        System.out.println("Last: " + list.getLast());
+
+        // Test remove
+        list.remove(1);
+        System.out.println("After remove index 1: " + java.util.Arrays.toString(list.toArray()));
+
+        // Test removeFirst and removeLast
+        list.removeFirst();
+        list.removeLast();
+        System.out.println("After removeFirst and removeLast: " + java.util.Arrays.toString(list.toArray()));
+
+        // Test exists
+        System.out.println("Exists 25: " + list.exists(25));
+        System.out.println("Exists 99: " + list.exists(99));
+
+        // Test indexOf and lastIndexOf
+        list.add(25);
+        list.add(25);
+        System.out.println("Index of 25: " + list.indexOf(25));
+        System.out.println("Last index of 25: " + list.lastIndexOf(25));
+
+        // Test sort
+        list.add(5);
+        list.add(100);
+        list.sort();
+        System.out.println("After sort: " + java.util.Arrays.toString(list.toArray()));
+
+        // Test clear and size
+        list.clear();
+        System.out.println("After clear: size = " + list.size());
+        System.out.println("Array after clear: " + java.util.Arrays.toString(list.toArray()));
+
+        // Test iterator
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.print("Using iterator: ");
+        for (int item : list) {
+            System.out.print(item + " ");
         }
+        System.out.println();
     }
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,159 @@
-public class MyArrayList {
+import java.util.Iterator;
+
+public class MyArrayList<T> implements MyList<T> {
+    private Object[] data;
+    private int size;
+    private static final int capacity=10;
+
+
+    public MyArrayList() {
+        this.data = new Object[capacity];
+        this.size = 0;
+    }
+    public void ensureCapacity() {
+        if(size == data.length){
+            Object[] newData = new Object[data.length*2];
+            for(int i=0;i<data.length;i++){
+                newData[i]=data[i];
+            }
+            data=newData;
+        }
+    }
+
+    public void checkIndex(int index) {
+        if(index<0 || index>=size){
+            throw new IndexOutOfBoundsException("out of bounds");
+        }
+    }
+
+    public void checkIndexForAdd(int index){
+        if(index<0 || index> size){
+            throw new IndexOutOfBoundsException("out of range for add");
+        }
+    }
+
+
+    public void add(T item) {
+        ensureCapacity();
+        data[size++]=item;
+    }
+
+    public void set(int index, T item) {
+        checkIndex(index);
+        data[index]=item;
+    }
+
+    public void add(int index, T item) {
+        checkIndexForAdd(index);
+        ensureCapacity();
+        for(int i = size; i>index;i--){
+            data[i]=data[i-1];
+        }
+        data[index]=item;
+        size++;
+    }
+
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    public void addLast(T item) {
+        add(item);
+    }
+
+    public T get(int index) {
+        checkIndex(index);
+        return (T) data[index];
+    }
+
+    public T getFirst() {
+        return get(0);
+    }
+
+    public T getLast() {
+        return get(size-1);
+    }
+
+    public void remove(int index) {
+        checkIndex(index);
+        for(int i=index;i<size-1;i++){
+            data[i+1]=data[i];
+        }
+        data[--size]=null;
+    }
+
+    public void removeFirst() {
+        remove(0);
+    }
+
+    public void removeLast() {
+        remove(size-1);
+    }
+
+    public void sort() {
+        for(int i =0; i<size-1;i++){
+            for(int j = i+1;j<size;j++){
+                Comparable<T> a = (Comparable<T>) data[i];
+                T b = (T) data[j];
+                if(a.compareTo(b)>0){
+                    Object temp = data[i];
+                    data[i]=data[j];
+                    data[j]=temp;
+                }
+            }
+        }
+
+    }
+
+    public int indexOf(Object object) {
+        for(int i =0;i<size-1;i++){
+            if(data[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object object) {
+        for(int i =size-1;i>=0;i--){
+            if(data[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    public boolean exists(Object object) {
+
+        return indexOf(object) != 1;
+    }
+
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        for(int i=0;i<size;i++){
+            array[i]=data[i];
+        }
+        return array;
+    }
+
+    public void clear() {
+        for(int i=0;i<size;i++){
+            data[i]=null;
+        }
+        size=0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<T>() {
+            private int CurrentInd = 0;
+            public boolean hasNext(){
+                return (CurrentInd < size);
+            }
+            public T next(){
+                return (T) data[CurrentInd++];
+            }
+
+        };
+    }
 }
